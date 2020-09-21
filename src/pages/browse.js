aa261af7cc8b6b5d9232558d21ddf089ce6e6a08
Rotcaermota <55855786+marko-gacic@users.noreply.github.com>
2020-09-21 03:40:36 +0200

    setting up user page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
+import selectionFilter from '../utils/selection-filter'
+
+
 
 export default function Browse() {
-    return <p>Hello from browse!</p>
+    //ubaciti serije i filmove
+
+    const { series } = useContent('series')
+    const { films } = useContent('films')
+    //ubaciti slajd
+
+    const slides = selectionFilter({ series, films })
+
+    //preneti na browse container
+    return <BrowseContainer slides={slides} />
+
 }
